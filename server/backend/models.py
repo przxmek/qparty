27d74c6261367983d3fd650a7b8633c667984eb6
Tomasz Laskowski <tomekl069@gmail.com>
2014-10-26 02:36:19 +0000

    Name in Song model added

--- server/backend/models.py
+++ server/backend/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Song(models.Model):
     service_id = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     voting_result = models.IntegerField(default=0)
 
 
